0575b17cea5150b978ac8eb7cab2d157486e8482
David Grant <davidgrant@gmail.com>
2014-06-29 00:05:55 -0700

    remove my api key, which I have no disabled, from the code

--- brickowl2rebrickable.py
+++ brickowl2rebrickable.py
@@ -8,17 +8,16 @@ import json
 
 from rebrickable_colors import get_color_conversion_table
 
-BRICK_OWL_API_KEY = 'f1ce0052456f75e0a8d8caafbe73b6c3bc2ed6df18433c71fd43b1f33bf9340a'
 URL = 'https://api.brickowl.com/v1/order/items?%s'
 
-def fetch_order(order_id):
-    params = urllib.parse.urlencode({'key': BRICK_OWL_API_KEY, 'order_id': order_id})
+def fetch_order(api_key, order_id):
+    params = urllib.parse.urlencode({'key': api_key, 'order_id': order_id})
     f = urllib.request.urlopen(URL % params)
     return f.read().decode('utf8')
 
-def create_rebrickable_csv(brick_owl_order_number):
+def create_rebrickable_csv(api_key, brick_owl_order_number):
     color_table = get_color_conversion_table()
-    brick_owl_order_json = json.loads(fetch_order(brick_owl_order_number))
+    brick_owl_order_json = json.loads(fetch_order(api_key, brick_owl_order_number))
     for item in brick_owl_order_json:
         print("****", item['name'])
         color_name = item['color_name']
@@ -42,15 +41,16 @@ def create_rebrickable_csv(brick_owl_order_number):
             writer.writerow([design_id, item['rebrickable_color_id'], item['ordered_quantity']])
 
 
-def create_rebrickable_csvs(order_ids):
+def create_rebrickable_csvs(api_key, order_ids):
     for order_id in order_ids:
-        create_rebrickable_csv(order_id)
+        create_rebrickable_csv(api_key, order_id)
 
 def main():
     parser = argparse.ArgumentParser(description='Convert Brickowl orders into Rebrickable csv files')
+    parser.add_argument('api_key', metavar='API_KEY', help='Brickowl API key')
     parser.add_argument('orders', metavar='ORDER_NUM', type=int, nargs='+', help='Brickowl order number(s)')
     args = parser.parse_args()
-    create_rebrickable_csvs(args.orders)
+    create_rebrickable_csvs(args.api_key, args.orders)
 
 if __name__ == '__main__':
     main()

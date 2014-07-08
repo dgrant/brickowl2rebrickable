00b017e45be2b0e52ab21ae525543759e9d965c9
David Grant <davidgrant@gmail.com>
2014-07-08 10:28:38 -0700

    removed some unused code

--- test_converter.py
+++ test_converter.py
@@ -1,30 +1,7 @@
 import unittest
-import mock
 from mock import Mock, MagicMock
-import urllib.request
-from urllib.parse import urlparse
 
 import converter
-
-class Matcher(object):
-    def __init__(self, compare, some_obj):
-        self.compare = compare
-        self.some_obj = some_obj
-
-    def __eq__(self, other):
-        return self.compare(self.some_obj, other)
-
-def compare(first, second):
-    first = urlparse(first)
-    second = urlparse(second)
-    if first.scheme == second.scheme \
-        and first.netloc == second.netloc \
-        and first.path == second.path \
-        and first.params == second.params \
-        and first.query == second.query:
-        return True
-    else:
-        return False
 
 class TestBrickOwl(unittest.TestCase):
     def setUp(self):
